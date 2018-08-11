9004f4882ae1c6014014fbf25f0d97dd8b9d48d2
Tomas Gonzalez Dowling <tomas@comtomtech.com>
2018-08-11 18:51:32 -0300

    agrega la opcion de copiar todo el listado para cada solapa del reporte

--- report_screen.py
+++ report_screen.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtGui import QIcon
 from ui.ui_report import Ui_Report
@@ -17,9 +18,35 @@ class ReportScreen(QDialog, Ui_Report):
 
         self.setWindowTitle("Actualizador de precios - Comtom Tech")
 
+        self.btn_copy_notmatched.clicked.connect(self.copyAll)
+        self.btn_copy_notfound.clicked.connect(self.copyAll)
+        self.btn_copy_notupdated.clicked.connect(self.copyAll)
+
+    def copyAll(self):
+        if self.sender().objectName() == 'btn_copy_notmatched':
+            widget = self.lst_notmatched
+        elif self.sender().objectName() == 'btn_copy_notfound':
+            widget = self.lst_notfound
+        else:
+            widget = self.lst_notupdated
+
+        items = ""
+        for index in range(widget.count()):
+            items += "%s\n" % widget.item(index).text()
+
+        cb = QApplication.clipboard()
+        cb.clear(mode=cb.Clipboard)
+        cb.setText(items, mode=cb.Clipboard)
+
     def updateValues(self, updated_list, failed_list, code_not_found_list, description_not_found_list, not_uptated_list):
         for item in code_not_found_list:
             self.lst_notfound.addItem(item)
+            # para usar qtablewidget
+            # rowPosition = self.lst_notfound.rowCount()
+            # self.lst_notfound.setColumnCount(2)
+            # self.lst_notfound.insertRow(rowPosition)
+            # self.lst_notfound.setItem(rowPosition, 0, QTableWidgetItem(item.split(',')[0]))
+            # self.lst_notfound.setItem(rowPosition, 1, QTableWidgetItem(item.split(',')[1]))
 
         for item in description_not_found_list:
             self.lst_notmatched.addItem(item)
